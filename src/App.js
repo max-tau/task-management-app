ae5fad932475965e6c01d51be23c037e1038f7f7
Max Tauroza <maxtauroza@gmail.com>
2023-07-24 11:14:24 +0100

    Install cors package and include code in App.js to make it work

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const cors = require("cors");
 const taskRouter = require("./routes/task");
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use("/task", taskRouter);
 
 module.exports = app;
